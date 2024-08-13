a7b75782ca580c550eb14970f04e8510e58d91ec
boxgaming <75969133+boxgaming@users.noreply.github.com>
2024-08-13 09:16:32 -0500

    Updated to dynamically determine whether to use "/" or "/qbjs/" prefix based on origin

--- service-worker.js
+++ service-worker.js
@@ -14,9 +14,9 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2';
+const PRECACHE = 'precache-v3';
 const RUNTIME = 'runtime';
-const PREFIX = "/qbjs/"; // "/";
+const PREFIX = (self.location.origin.indexOf("github.io") == -1) ? "/" : "/qbjs/";
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
