602e53e15e393a0f568a353343fde4ba9229416c
boxgaming <75969133+boxgaming@users.noreply.github.com>
2026-02-06 17:59:33 -0600

    cache version updates

--- service-worker.js
+++ service-worker.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v72';
-const RUNTIME = 'runtime-v10';
+const PRECACHE = 'precache-v73';
+const RUNTIME = 'runtime-v11';
 const PREFIX = (self.location.origin.indexOf("github.io") == -1) ? "/" : "/qbjs/";
 
 // A list of local resources we always want to be cached.
